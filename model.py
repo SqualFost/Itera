1cf1153461cbecae620243000d36ff1a6d8891ba
SqualFost <alertematteo@gmail.com>
2026-04-14 20:41:20 +0200

    add of functions to list availables models and chat with the LLM

--- model.py
+++ model.py
@@ -1,6 +1,27 @@
 from ollama import chat
 import ollama
 
-for m in ollama.list()['models']:
-    print(m['model'])
 
+#List all availables models
+def list_models():
+    p = []
+    try:
+        for m in ollama.list()['models']:
+            p.append(m['model'])
+        return p
+    except Exception:
+        return []
+
+#Chat with LLM, takes in argument the model that has been chosen, and the user input
+def model_chat(text: str, model: str):
+    stream = chat(
+        model=model,
+        messages=[{'role': 'user', 'content': f"{text}"}],
+        stream=True,
+    )
+    for chunk in stream:
+        print(chunk['message']['content'], end='', flush=True)
+
+print(list_models())
+
+model_chat('salut ca va ?', 'gemma4:e4b')
